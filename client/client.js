ec8eb014f0fe296eda9113195189d55ccab7697e
LB (Ben) Johnston <lb@doubleampersand.com.au>
2014-07-29 08:09:35 +1000

    Initial Commit

---
+++ client/client.js
@@ -0,0 +1,8 @@
+Template.main.rendered = function() {
+  // Menu settings
+  $('#menuToggle, .menu-close').on('click', function(){
+    $('#menuToggle').toggleClass('active');
+    $('body').toggleClass('body-push-toleft');
+    $('#theMenu').toggleClass('menu-open');
+  });
+};
