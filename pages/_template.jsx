ffb16993696ba69ad21c5ab72014cd73ad1f80fa
LB <mail@lb.ee>
2017-02-23 13:48:22 +0700

    Trying out elementalui
    This is after trying about three other ways to get styling :)

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+// import { Container } from 'react-responsive-grid'
 // import { Link } from 'react-router'
 // import { prefixLink } from 'gatsby-helpers'
 
 // Import styles.
-import 'css/main.sass'
+// import 'css/main.sass'
 
 // import { rhythm } from '../utils/typography'
+// import Flex from 'react-uikit-flex';
+// import Panel from 'react-uikit-panel';
+import '../node_modules/elemental/less/elemental.less'
+import { Row, Col } from 'elemental'
 
 module.exports = React.createClass({
   propTypes () {
@@ -15,12 +20,14 @@ module.exports = React.createClass({
   },
   render () {
     return (
-      <div id="parent" className="columns container is-fluid">
-        <div className="column is-one-quarter">Menu</div>
-        <div className="column">
+      <Row>
+        <Col xs="100%" sm="25%" lg="33.333%">
+          Menu
+        </Col>
+        <Col xs="100%" sm="77%" lg="66.667%">
           {this.props.children}
-        </div>
-      </div>
+        </Col>
+      </Row>
     )
   },
 })
