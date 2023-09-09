2c70123962425223f1e7ce3a21493ca62900afc7
LB Johnston <lb.johnston@virginaustralia.com>
2023-09-09 11:36:03 +1000

    Initial migration to 11ty

---
+++ _data/metadata.js
@@ -0,0 +1,12 @@
+module.exports = {
+	title: "Welcome | lb.ee",
+	url: "https://lb.ee/",
+	language: "en-AU",
+	description:
+		"Website of LB (Ben) Johnston. Absolutely passionate to understand how things work, create and improve systems.",
+	author: {
+		name: "LB (Ben) Johnston",
+		email: "mail@lb.ee",
+		url: "https://lb.ee/",
+	},
+};
--- src/htmlScripts/loadCSS.js
+++
@@ -1,8 +0,0 @@
-const js = `
-window.bestofjs = {};
-function loadCSS(e,t,n){"use strict";var i=window.document.createElement("link");var o=t||window.document.getElementsByTagName("script")[0];i.rel="stylesheet";i.href=e;i.media="only x";o.parentNode.insertBefore(i,o);setTimeout(function(){i.media=n||"all"})}
-loadCSS('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-loadCSS('https://fonts.googleapis.com/css?family=Josefin+Slab');
-loadCSS('https://fonts.googleapis.com/css?family=Oxygen');
-`
-export default js
